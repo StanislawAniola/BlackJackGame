320022106d450115bd08b7b4116c8b0107214147
Stanisław Anioła <stanislaw.aniola@dataedo.com>
2021-02-23 23:57:38 +0100

    src/black-jack-main.cpp: added objects to make deck of card

--- src/black-jack-main.cpp
+++ src/black-jack-main.cpp
@@ -1,8 +1,13 @@
 #include "iostream"
 #include "string"
+#include "map"
+#include "vector"
 
 int main()
 {
-    std::cout << "Hello World\n";
+    std::vector <std::string> suits = {"Hearts", "Dimonds", "Spades", "Clubs"};
+    std::vector <std::string> ranks = {"Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King", "Ace"};
+    std::map <std::string, int> {{"Two", 2}, {"Three", 3}, {"Four", 4}, {"Five", 5}, {"Six", 6}, {"Seven", 7}, {"Eight", 8}, {"Nine", 9}, {"Queen", 10}, {"King", 10}, {"Ace", 11}};
+
     return 0;
 }
